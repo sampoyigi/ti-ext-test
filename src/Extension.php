2333bd5789abae6334bb54962b0c93ded762da29
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-11-12 14:24:43 +0000

    wip

--- src/Extension.php
+++ src/Extension.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace SamPoyigi\Test;
 
 use Igniter\System\Classes\BaseExtension;
+use Override;
 
 class Extension extends BaseExtension
 {
+    #[Override]
     public function boot() {}
 }
